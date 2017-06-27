2ef22a4a9f9b6591b21434b453ee1928792497ab
shiotomo <t.shiota16@gmail.com>
2017-06-28 00:44:30 +0900

    Add ip

--- .zsh.d/prompt.zsh
+++ .zsh.d/prompt.zsh
@@ -1,2 +1,3 @@
-PROMPT='%F{cyan}[%n@%m:%f%F{yello}%~%f%F{cyan}]%f %D{[%m/%d-%T]}
+ip=`hostname -I | cut -f1 -d' '`
+PROMPT='%F{cyan}[%n@%m:%f%F{yello}%~%f%F{cyan}]%f %F{green}[$ip]%f %D{[%m/%d-%T]}
 %F{cyan}>>%f '
