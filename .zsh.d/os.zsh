f12ad615bd0ffb522ea2b72f7807e81bc21cf7bd
shiotomo <t.shiota16@gmail.com>
2017-07-11 15:33:47 +0900

    Add open

--- .zsh.d/os.zsh
+++ .zsh.d/os.zsh
@@ -10,4 +10,5 @@ case ${OSTYPE} in
     alias csc='/cygdrive/c/WINDOWS/MICROSOFT.NET/Framework/v4.0.30319/csc'
     alias python='python3'
     alias python_x='run xwin-multiwindow && export DISPLAY=:0.0'
+    alias open='cygstart'
 esac
